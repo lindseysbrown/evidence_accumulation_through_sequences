7b6c89625420c5e35670e8d9894f1849ef0d3b42
Lindsey Brown <lindseyb@princeton.edu>
2024-01-18 16:34:45 -0500

    Comments for Figure S2

--- FigureS2/psychometrics_S2B.py
+++ FigureS2/psychometrics_S2B.py
@@ -17,12 +17,11 @@ matplotlib.rcParams['pdf.fonttype'] = 42
 matplotlib.rcParams['ps.fonttype'] = 42
 matplotlib.rc('font',**{'family':'sans-serif','sans-serif':['Arial']})
 
-trialdata = pd.read_pickle('trialdata.pkl')
+trialdata = pd.read_pickle('trialdata.pkl') #file containing data for set of trials with positions of left and right cues on each trial
 
 #parameters
-a = 1
-b = a
-c = 0
+a = 1 #rate of decay and self-excitation
+b = a #feedforward connections
 Plength = 20
 
 T = 300
@@ -37,136 +36,108 @@ for i in range(1, neurons):
     #feedfoward connections
     W[i, i-1] = b
     W[i+neurons, i-1+neurons] = b
-    #inhibitory connections
-    W[i+neurons, i] = -c
-    W[i, i+neurons] = -c
-
 
 def P(t):
+    '''
+    Position gating signal, equal to T when neuron is active, assuming constant velocity
+    '''
     pos = np.zeros((neurons,))
     pos[int(np.floor(t/Plength))] = T
     return np.concatenate((pos, pos))
 
 def I(t, Lcues, Rcues):
+    '''
+    External input drive from cues, that contribute a square pulse if a cue occurs within .5cm of the current position
+
+    ===INPUTS===
+    t: current position
+    Lcues: list of positions at which left towers occur
+    Rcues: list of positions at which right towers occur
+    '''
     Lcue = np.abs(t-Lcues)
     if min(Lcue)<.5:
-        IL = 4*np.ones((neurons,))
+        IL = np.ones((neurons,))
     else:
         IL = np.zeros((neurons,))
     Rcue = np.abs(t-Rcues)
     if min(Rcue)<.5:
-        IR = 4*np.ones((neurons,))
+        IR = np.ones((neurons,))
     else:
         IR = np.zeros((neurons,))    
     return np.concatenate((IL-IR, IR-IL))
 
 def correct(sol, Lcues, Rcues):
+    '''
+    Returns boolean of whether the final neuron in the chain with more inputs had greater firing rate
+    '''
     if len(Lcues)>len(Rcues):
         return sol[-1,16]>sol[-1, 33]
     return sol[-1, 16]<sol[-1, 33]
 
 def simulate(Lcues, Rcues, input_noise = False, Inoise = .67):
+    '''
+    Simulate a single trial for a set of input towers
+
+    ===INPUTS===
+    Lcues: list of positions at which left towers occur
+    Rcues: list of positions at which right towers occur
+
+    '''
     #reset simulation
     Lchain = np.zeros((neurons,))
     Rchain = np.zeros((neurons,))
     
-    Lchain[0] = 65
-    Rchain[0] = 65
+    Lchain[0] = 16.25
+    Rchain[0] = 16.25
     
     Lcues= Lcues+30
     Rcues = Rcues+30
     
-    if input_noise:
+    if input_noise: #option for not integrating some cues in the case of input noise
         Lkeep = np.random.uniform(0, 1, size = len(Lcues))
         Lcues[Lkeep<Inoise] =  500
         Rkeep = np.random.uniform(0, 1, size = len(Rcues))
         Rcues[Rkeep<Inoise] =  500
-        
-    
 
-    def chain(y, t):
+    def chain(y, t): #differntial equation, Eq. (S2)
         dydt = -a*y+np.maximum(W@y+P(t)+I(t, Lcues, Rcues)-T, 0)
         return dydt
-
-
 
     y0 = np.concatenate((Lchain, Rchain))
     
     t = np.linspace(0, 330, 3301)
     
-    sol = odeint(chain, y0, t, hmax=5)
+    sol = odeint(chain, y0, t, hmax=5) #numerically integrate with scipy's odeint
     return sol
 
-
-
-
-#without noise in input
-#leftsol = np.zeros((3301, neurons*2))
-#rightsol = np.zeros((3301, neurons*2))
-#alldata = np.zeros((3301, neurons*2))
-#lchoices = []
-#rchoices = []
-psychometric = {}
-    
-for t in range(len(trialdata)):
+#get psychometric curve for 1000 trials with no noise in input
+psychometric = {}    
+for t in range(len(trialdata)): #loop over all trials in trialdata
     Lcues = trialdata['leftcues'][t]
-    Lcues = np.append(Lcues, 500)
+    Lcues = np.append(Lcues, 500) #append cue to the end to avoid empty arrays
     Rcues = trialdata['rightcues'][t]
     Rcues = np.append(Rcues, 500)
     sol = simulate(Lcues, Rcues)
-    delta = len(Lcues)-len(Rcues)
+    delta = len(Lcues)-len(Rcues) #final cue difference
     
-    
- #   if len(Lcues)>len(Rcues):
-  #      leftsol = np.dstack((leftsol, sol))
-    
-   # if len(Rcues)>len(Lcues):
-    #    rightsol = np.dstack((rightsol, sol))
-    
-    #collect all data to parallel neural analysis
-    #alldata = np.dstack((alldata, sol))
-    
+    #keep track of trials for which left or right decisions were made
     wentleft = 1*(sol[-1, 16]>sol[-1, 33])
-    #wentright = 1*(sol[-1, 16]<sol[-1, 33])
-    #if wentleft:
-     #   lchoices.append(t)
-    #if wentright:
-    #    rchoices.append(t)
-        
+
+    #track whether the final decision was left or right for different differences in cues     
     if delta in psychometric:
         psychometric[delta].append(wentleft)
     else:
         psychometric[delta] = [wentleft]
 
-
-
+#calculate pyschometric curve        
 psychometric.pop(0, 0)
 cuediffsnonoise = sorted(psychometric.keys())
 perfnonoise = [np.mean(psychometric[c]) for c in cuediffsnonoise]
 
-#remove initialized zero array        
-#leftsol = leftsol[:, :, 1:]
-#rightsol = rightsol[:, :, 1:]
-#alldata = alldata[:, :, 1:]
-
-
-
-#plt.figure()
-#plt.imshow(np.mean(rightsol,axis=2).T, aspect = 'auto', cmap = 'Greys', origin='lower')
-#plt.title('Right Choice Trials')
-#plt.xlabel('Position')
-#plt.ylabel('Neuron')
-#plt.savefig('SeqPlotRightCompetingChains.pdf', transparent=True)
-
-#plt.figure()
-#plt.imshow(np.mean(leftsol,axis=2).T, aspect = 'auto', cmap = 'Greys', origin='lower')
-#plt.title('Left Choice Trials')
-#plt.xlabel('Position')
-#plt.ylabel('Neuron')
-#plt.savefig('SeqPlotLeftCompetingChains.pdf', transparent=True)
-
-
 def rebin(p):
+    '''
+    function that averages a dictionary of psychometric data collected for single evidence levels and rebins as groups of 3 evidence levels
+    '''
     pnew= {}
     for i in range(-14, 14, 3):
         pnew[i] = []
@@ -186,23 +157,21 @@ def rebin(p):
             pnew.pop(i)
     return pnew
 
-
 #with noise in input
 psychometrics = []
 
-for i in range(25):
+for i in range(25): #for 25 sessions
     psychometricerror = {}
     
-    samples = np.random.choice(np.arange(1000), 150)
+    samples = np.random.choice(np.arange(1000), 150) #randomly select 150 trials for session
         
-    for t in samples:
+    for t in samples: #loop over selected trials, simulating with input noise
         Lcues = trialdata['leftcues'][t]
         Lcues = np.append(Lcues, 500)
         Rcues = trialdata['rightcues'][t]
         Rcues = np.append(Rcues, 500)
-        sol = simulate(Lcues, Rcues, input_noise = True)
+        sol = simulate(Lcues, Rcues, input_noise = True) #set input noise to true for simulation with cues ignored
         delta = len(Lcues)-len(Rcues)
-        
         
         wentleft = 1*(sol[-1, 16]>sol[-1, 33])    
         if delta in psychometricerror:
@@ -213,6 +182,7 @@ for i in range(25):
     psychometricerror.pop(0, 0)
     psychometrics = psychometrics + [psychometricerror]
 
+#rebin evidence levels for noisy simulated sessions
 rebinpsychometrics = [rebin(p) for p in psychometrics]
 perfs = []
 for p in rebinpsychometrics:
@@ -229,14 +199,14 @@ cuediffs = sorted(performance.keys())
 meanperf = [np.mean(performance[c]) for c in cuediffs]
 sdperf = [sem(performance[c]) for c in cuediffs]
 
+#plot psychometric curves
 plt.figure()
-plt.plot(-1*np.array(cuediffsnonoise), 1-np.array(perfnonoise), color='black', label = 'No Noise')
-plt.errorbar(-1*np.array(cuediffs), 1-np.array(meanperf), yerr = sdperf, color = 'red', label = 'Input Noise')
+plt.plot(-1*np.array(cuediffsnonoise), 1-np.array(perfnonoise), color='black', label = 'No Noise') #psychometric data without noise
+plt.errorbar(-1*np.array(cuediffs), 1-np.array(meanperf), yerr = sdperf, color = 'red', label = 'Input Noise') #psychometric data from noisy simulated sessions
 plt.xlabel('#R - #L')
-plt.ylabel('Model Performance')
 plt.ylim([0,1])
 plt.xlim([-15, 15])
 plt.axvline(x=0, color = 'grey', linestyle = 'dashed') 
 plt.axhline(y=0.5, color = 'grey', linestyle = 'dashed')  
+plt.ylabel('Model Performance')
 plt.legend()
-plt.savefig('LRSwitch-July12PsychometricCompetingInputs.pdf', transparent = True)
